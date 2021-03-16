750c81d3ef00f45d0d25b59ef30da5a5575ca3f9
Brayden England <bray.patriot@gmail.com>
2021-03-16 17:26:42 -0600

    Moved server file up a directory

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = 3001
 
 // body parser
 app.use(express.urlencoded({ extended: true }))
 
 // static folder
-app.use(express.static('../build'))
+app.use(express.static('build'))
 
 // react index.html file
 //const reactPath = __dirname + '/build/index.html'
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/build/index.html')
 })
 
 app.get('/api/', (req, res) => {
